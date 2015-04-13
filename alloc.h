dd551156d1a976f69772b11fb6598c44a731d2f8
Filisha <filishaworks@gmail.com>
2015-04-12 20:56:52 -0700

    Update alloc.h

--- alloc.h
+++ alloc.h
@@ -1,10 +1,4 @@
-// UCLA CS 111 Lab 1 storage allocation
 
-////////////////////////////////////
-//Authors:
-//  Shubham Joshi   :   504-134-654
-//  Derek Foster    :   504-146-063
-////////////////////////////////////
 
 #include <stddef.h>
 void *checked_malloc (size_t);
